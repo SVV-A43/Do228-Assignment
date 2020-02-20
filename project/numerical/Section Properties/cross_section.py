606db42a3e6f7ba8f4624cc079529b89f1f34d30
Kevin <kevbislip@hotmail.com>
2020-02-20 16:51:26 +0100

    Variable and value integrator for polynomials and sin/cos

--- project/numerical/Section Properties/cross_section.py
+++ project/numerical/Section Properties/cross_section.py
@@ -64,27 +64,88 @@ x_centroid = (stiff_cont+spar_cont+semi_cont+straight_cont)/A_total
 
 
 # Shear centre
-
-def res_stringer(s, stiff_locs):
-    sum = 0
-    for stringer in stiff_locs:
-        if s > stringer[si]:
-            sum += stringer[area] * stringer[yi]
+"""
+Due to symmetry Izy = 0. Shear centre must be located on the z-axis. To find the shear center location eta in the z-axis
+A unit load Sy and Sz = 0 is applied. One cut is made in the spar and one in the circular section and qb = 0 at the cuts
 
 
-qb = np.zeros(sz)  # contains all qbs for each
-for i in range(1, sz):
-    qb[i] = -(1/Izz)*( t*num_int(y[i] + res_stringer(s, stiff_locs)) + qb[i-1] )
+"""
+I_zy = 0
+I_zz = 1
+I_yy = 1
+Vy = 1
+Vx = 0
+
+def variable_integrator(func):
+    """func is a list with lists containing the constants in the first list, and coefficients in the second list
+    if coefficient is not a number, i.e. sin it should be written as s, if it cosine it is c"""
+    constants = func[0]
+    coefficients = func[1]
+    for i in range(len(coefficients)):
+        if type(coefficients[i]) == int:
+            constants[i] *= 1/(coefficients[i]+1)
+            coefficients[i] += 1
+
+        elif coefficients[i] == 's':
+            constants[i] *= -1
+            coefficients[i] = 'c'
+
+        elif coefficients[i] == 'c':
+            constants[i] *= 1
+            coefficients[i] = 's'
+    return [constants,coefficients]
+
+def value_integrate(a,b,func):
+    """Integrates a function going from a to b"""
+    constants = func[0]
+    coefficients = func[1]
+    result = 0
+    for i in range(len(coefficients)):
+        if type(coefficients[i]) == int:
+            constants[i] *= 1/(coefficients[i]+1)
+            coefficients[i] += 1
+            result += (constants[i]*b**coefficients[i] - constants[i]*a**coefficients[i])
+
+        elif coefficients[i] == 's':
+            constants[i] *= -1
+            coefficients[i] = 'c'
+            result += constants[i]*(m.cos(b) - m.cos(a))
+
+
+        elif coefficients[i] == 'c':
+            constants[i] *= 1
+            coefficients[i] = 's'
+            result += constants[i]*(m.sin(b) - m.sin(a))
+    return result
+
+
+a = variable_integrator([[2,4],[1,2]]) #2s + 4s^2
+b = value_integrate(0,m.pi/2,[[2],['c']]) #2costheta from 0 to pi/2
+# qb of section 1:
+# def qb_in_variable_form():
+
+
+# def res_stringer(s, stiff_locs):
+#     sum = 0
+#     for stringer in stiff_locs:
+#         if s > stringer[si]:
+#             sum += stringer[area] * stringer[yi]
+#
+#
+# qb = np.zeros(sz)  # contains all qbs for each
+# for i in range(1, sz):
+#     qb[i] = -(1/Izz)*( t*num_int(y[i] + res_stringer(s, stiff_locs)) + qb[i-1] )
 
 # Moments of inertia
 
-'''    
-plt.plot(stiff_loc[:,0],stiff_loc[:,1], marker="o")    
-plt.plot(x,y)
-plt.plot(x_centroid,0, marker="o")
-plt.axis('equal')
-plt.show()
-'''
+
+# plt.plot(stiff_loc[:,0],stiff_loc[:,1], marker="o")
+# plt.plot(x,y)
+# plt.plot(x,-y)
+# plt.plot(x_centroid,0, marker="o")
+# plt.axis('equal')
+# plt.show()
+
 
 print("perimeter =", perimeter)
 print(stiff_loc)
