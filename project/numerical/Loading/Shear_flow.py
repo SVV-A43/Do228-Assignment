8d14b4842686f02e74f8aa113031fa55aaee6615
Kevin <kevbislip@hotmail.com>
2020-02-24 17:19:13 +0100

    shear flow

--- project/numerical/Loading/Shear_flow.py
+++ project/numerical/Loading/Shear_flow.py
@@ -4,7 +4,20 @@ import matplotlib.pyplot as plt
 from project.numerical.Loading.integration import def_integral, variable_integral
 
 
+# Real values
+C = 0.515
+h = 0.248
+n = 11
+w_st = 0.03
+h_st = 0.015
+t_st = 0.0012
+t_skin = 0.0011
+t_spar = 0.0022
 
+I_zz = 1.4221372629975417e-5
+I_yy = 5.643650631210155e-5
+
+# Test values
 
 h = 2
 t_skin = 1
@@ -23,22 +36,75 @@ l_sk = m.hypot(C-h/2,h/2)
 def qb_2_1(s):
     return -s*Vy*t_skin/I_zz
 def qb_2_2(s):
-    return s
+    return  np.array([[-eta*Vz*t_skin/I_yy]])
 
-def_integral(fn_test1, start, end, num_bins=100)
+# qb_2_val = def_integral(qb_2_1, 0, 1, num_bins=100) + def_integral(qb_2_2, 0, 1, num_bins=100)
 
-qb_2_Vy = value_integrate(0,h/2,[[-Vy*t_skin/I_zz],[1]])
-qb_2_Vz = value_integrate(0,h/2,[[-Vz*t_skin*eta/I_zz],[0]])
-qb_2 = Func(qb_2_Vy[0],qb_2_Vy[1]).__add__(qb_2_Vz)
-qb_2_val = qb_2.get_const()
-qb_2_var = qb_2.get_var()
-print(qb_2)
-print(qb_2_val,qb_2_var)
+
 
 # Shear flow 1,3,4,6
-def qb_2_1(s):
+dx = 0.0001
+x = np.arange(0, C+dx,dx)
+x = np.append(x, x[::-1])
+sz = np.size(x)
+y = np.zeros(sz)
+stiff_loc = np.array([[ 0.        ,  0.        ],
+                      [ 0.0455    ,  0.09598828],
+                      [ 0.1479    ,  0.11642046],
+                      [ 0.2527    ,  0.08318465],
+                      [ 0.3576    ,  0.04991714],
+                      [ 0.4624    ,  0.01668133],
+                      [ 0.4624    , -0.01668133],
+                      [ 0.3576    , -0.04991714],
+                      [ 0.2527    , -0.08318465],
+                      [ 0.1479    , -0.11642046],
+                      [ 0.0455    , -0.09598828]])
+
+s_list = [m.pi/2, l_sk, l_sk, m.pi/2]  #list containing lengths of each skin 1,3,4,6
+s_current = 0
+seg_i = 0  #0: 1, 1: 3, 2: 4, 3: 6
+s_last = -10**4
+
+"""
+Go through outer section, using x, y find if the current location is equal to a stiffener position, if true add to the correct segment.
+Change segments by keeping track of perimeter, if perimeter exceeds the length a segment, then switch to next segment.
+"""
+func_list = [[qb_1_1,qb_1_2],[]]
+qb_val_list = [qb_2_val]
+
+for i in range(sz):  # go through the outer section
+
+    # Switch segments
+    if s_current > s_list[seg_i]:
+        seg_i += 1
+        s_current = 0
+
+    if seg_i == 0:
+        y[i] = m.sqrt((h/2)**2-(x[i]-h/2)**2)
+    elif seg_i == 1:
+        y[i] = -((h/2)/(C-h/2))*x[i] + (((h/2)/(C-h/2))*C)
+    elif seg_i == 2:
+        y[i] = -(-((h/2)/(C-h/2))*x[i] + (((h/2)/(C-h/2))*C))
+    elif seg_i == 3:
+        y[i] = -m.sqrt((h/2)**2-(x[i]-h/2)**2)
+
+    # Numerically integrate qb
+    qb_current = def_integral(func_list[seg_i][0], 0, s_list[seg_i], num_bins=100) + def_integral(func_list[seg_i][1], 0, s_list[seg_i], num_bins=100)
+    qb_val_list.append(qb_current)
+
+
+    current_loc = np.array(x[i], y[i])
+    if current_loc in stiff_loc:
+        #add to q
+
+    s_current += m.hypot(dx, y[i]-y[i-1])
+
+
+
+
+def qb_1_1(s):
     return s
-def qb_2_2(s):
+def qb_1_2(s):
     return s
 
 def qb_2_1(s):
@@ -61,44 +127,3 @@ def qb_2_1(s):
     return s
 def qb_2_2(s):
     return s
-
-
-# # lists are ordered as 1,3,4,6,5, which the numbers are given in cross section from verification model
-s_list = [h/2, m.pi/2]  #list containing lengths of each skin
-func_list = [func1,func2  ]
-final_qb_values_list = []
-final_qb_variables_list = []
-perimeter_addition = 0
-new_qb = Func([],[])
-s_current = 0
-seg_i = 0
-s_last = -10**4
-for i in range(sz):  # go through the outer section
-    if x[i] <= h/2:
-        y[i] = m.sqrt((h/2)**2-(x[i]-h/2)**2)
-    else:
-        y[i] = -((h/2)/(C-h/2))*x[i] + (((h/2)/(C-h/2))*C)
-
-    perimeter_old = perimeter_addition
-    perimeter_addition += m.hypot(dx, y[i]-y[i-1])
-    s_current += m.hypot(dx, y[i]-y[i-1])
-    if perimeter_addition > spacing > perimeter_old:
-        if perimeter_addition - spacing <= perimeter_old - spacing:
-            new_qb.__add__([[A_stiff*y[i]],[0]])
-        else:
-            new_qb.__add__([[A_stiff*y[i-1]],[0]])
-
-        perimeter_addition -= spacing
-        new_qb.__add__(qb_i(s_current, s_current+dx, func_list[i]))
-    else:
-        new_qb.__add__(qb_i(s_current, s_current+dx, func_list[i]))
-
-        perimeter_addition -= spacing
-    if s_current - s_list[seg_i] < dx*2 and s_current - s_last > dx*4:
-        s_last = s_current
-        seg_i += 1
-        final_qb_values_list.append(new_qb.get_func()[0])
-        final_qb_variables_list.append(new_qb.get_func()[1])
-
-
-print(final_qb_variables_list, final_qb_values_list)
